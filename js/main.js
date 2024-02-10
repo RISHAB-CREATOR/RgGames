9cbda605484a5b036a845102bdbc70bebef89643
Rishab <84559232+RISHAB-CREATOR@users.noreply.github.com>
2024-02-11 00:14:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ function check() {
   if ($("#panicmode").length > 0) {
     $("#panicmode").prop({ href: panicurl });
   }
-  if ($(".seleniteminified").length > 0) {
+  if ($(".RgGamesminified").length > 0) {
     $.get(
-      "https://raw.githubusercontent.com/skysthelimitt/selenite-optimized/main/build/bookmark.txt",
+      "https://raw.githubusercontent.com/RISHAB-CREATOR/RgGames-optimized/main/build/bookmark.txt",
       function (data) {
-        $(".seleniteminified").prop({ href: data });
+        $(".RgGamesminified").prop({ href: data });
       }
     );
   }
@@ -17,42 +17,42 @@ document.addEventListener('DOMContentLoaded', function() {
   const iconSetting = document.querySelector("input#discordIcon");
   const blockClose = document.querySelector("input#blockClose");
   if(localStorage.getItem("theme")) {
-    localStorage.setItem("selenite.theme", localStorage.getItem("theme"));
+    localStorage.setItem("RgGames.theme", localStorage.getItem("theme"));
     localStorage.removeItem("theme");
   }
-  if (localStorage.getItem("selenite.theme")) {
-    document.body.setAttribute("theme", localStorage.getItem("selenite.theme"));
+  if (localStorage.getItem("RgGames.theme")) {
+    document.body.setAttribute("theme", localStorage.getItem("RgGames.theme"));
   } else {
     document.body.setAttribute("theme", "main");
   }
   if(document.querySelector("widgetbot-crate")) {
-    if(localStorage.getItem("selenite.discordIcon") == "false") {
+    if(localStorage.getItem("RgGames.discordIcon") == "false") {
       const widget = document.querySelector("widgetbot-crate");
       widget.setAttribute("style", "display:none");
     }
   }
   if(document.querySelector("input#discordIcon")) {
-    if(localStorage.getItem("selenite.discordIcon") == "false") {
+    if(localStorage.getItem("RgGames.discordIcon") == "false") {
       iconSetting.checked = false;
     }
     iconSetting.addEventListener("click", () => {
-      localStorage.setItem("selenite.discordIcon", iconSetting.checked)
+      localStorage.setItem("RgGames.discordIcon", iconSetting.checked)
     });
   }
   if(document.querySelector("input#blockClose")) {
-    if(localStorage.getItem("selenite.blockClose") == "true") {
+    if(localStorage.getItem("RgGames.blockClose") == "true") {
       blockClose.checked = true;
     }
     blockClose.addEventListener("click", () => {
-      localStorage.setItem("selenite.blockClose", blockClose.checked)
+      localStorage.setItem("RgGames.blockClose", blockClose.checked)
     });
   }
   if(document.querySelector("input#tabDisguise")) {
-    if(localStorage.getItem("selenite.tabDisguise") == "true") {
+    if(localStorage.getItem("RgGames.tabDisguise") == "true") {
       tabDisguise.checked = true;
     }
     tabDisguise.addEventListener("click", () => {
-      localStorage.setItem("selenite.tabDisguise", tabDisguise.checked)
+      localStorage.setItem("RgGames.tabDisguise", tabDisguise.checked)
     });
   }
 
@@ -88,7 +88,7 @@ function copyToClipboard(text) {
 }
 
 function setTheme(theme) {
-  localStorage.setItem("selenite.theme", theme);
+  localStorage.setItem("RgGames.theme", theme);
   document.body.setAttribute("theme", theme);
 }
 function setPanicMode() {
@@ -102,6 +102,6 @@ function customTheme() {
   const customMenu = document.querySelector('#customMenu');
   customMenu.removeAttribute("display");
 }
-  if(location.hostname.includes(".vercel.app") && (location.hostname.includes("selenite-beta") || location.hostname.includes("space-lovers") || location.hostname.includes("school-education"))) {
-    alert("IMPORTANT:\nVercel links will be taken offline eventually. Please move to a new link. You can find these in the Discord at discord.gg/7jyufnwJNf, or you can try a few of the ones listed\nselenite.cc\nselenite.pages.dev\nselenite.skysthelimit.dev\nall-about-science.onrender.com")
+  if(location.hostname.includes(".vercel.app") && (location.hostname.includes("RgGames-beta") || location.hostname.includes("space-lovers") || location.hostname.includes("school-education"))) {
+    alert("IMPORTANT:\nVercel links will be taken offline eventually. Please move to a new link. You can find these in the Discord at discord.gg/7jyufnwJNf, or you can try a few of the ones listed\nRgGames.cc\nRgGames.pages.dev\nRgGames.skysthelimit.dev\nall-about-science.onrender.com")
   }
